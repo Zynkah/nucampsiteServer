2bb7e6c20df15ee0e7b6cb14af80b0b5de5fbec5
Zynkah <ffgurl07@yahoo.com>
2022-12-28 18:33:38 -0800

    workshop 3 progress

--- routes/campsiteRouter.js
+++ routes/campsiteRouter.js
@@ -197,18 +197,15 @@ campsiteRouter
   .put(authenticate.verifyUser, (req, res, next) => {
     Campsite.findById(req.params.campsiteId)
       .then((campsite) => {
-
-
         if (
           !req.user._id.equals(
-            campsite.comments.id(req.user._id).author(req.comments.id)
+            campsite.comments.id(req.params.commentId).author._id
           )
         ) {
           err = new Error("Incorrect User");
           err.status = 403;
           return next(err);
         }
-
 
         if (campsite && campsite.comments.id(req.params.commentId)) {
           if (req.body.rating) {
