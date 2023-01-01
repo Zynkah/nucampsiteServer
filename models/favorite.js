f72d34f2b7070f714f740701ae9f009fd2b200b5
Zynkah <ffgurl07@yahoo.com>
2022-12-31 17:22:59 -0800

    Workshop Week 4 Progress

--- models/favorite.js
+++ models/favorite.js
@@ -1,23 +1,18 @@
 const mongoose = require("mongoose");
 const Schema = mongoose.Schema;
 
-const favoriteSchema = new Schema(
-  {
-    user: {
-      type: mongoose.Schema.Types.ObjectId,
-      ref: "User",
-    },
-    campsites: [
-      {
-        type: mongoose.Schema.Types.ObjectId,
-        ref: "Campsites",
-      },
-    ],
+const favoriteSchema = new Schema({
+  user: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: "User",
   },
-  {
-    timestamps: true,
-  }
-);
+  campsites: [
+    {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "Campsite",
+    },
+  ],
+});
 
 const Favorite = mongoose.model("Favorite", favoriteSchema);
 
